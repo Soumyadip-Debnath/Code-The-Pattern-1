d564ee2df9ed652eac4e078ffce8bb137ab168f9
Riju-Code <116337655+Riju-Code@users.noreply.github.com>
2022-10-23 01:19:52 +0530

    Sample Pattern

---
+++ CPP-Patterns/ptrn1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int i, j;
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
